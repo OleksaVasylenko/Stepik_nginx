29a948963087bcead1e0454a0663c8593b5df217
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-07-18 17:10:29 +0300

    Update hello.py

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,6 +1,12 @@
-bind = '0.0.0.0:8080'
-workers = 9
-user = 'www'
-group = 'www'
-logfile = '/home/box/web/gunicorn.log'
-loglevel = 'info'
+CONFIG = {
+  'mode': 'wsgi',
+  'python': '/usr/bin/python',
+  'working_dir': '/home/box/web',
+  'args': (
+    '--bind=0.0.0.0:8000',
+    '--workers=2',
+    '--timeout=15',
+    '--log-level=debug',
+    'ask.wsgi:application',
+  ),
+}
