fa837bb5486e3057ca1afdde2a3c06a4ef476993
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-08-03 13:54:56 +0300

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -4,3 +4,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/django
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
