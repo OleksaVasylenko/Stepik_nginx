2ac4e8f2d90f125b8ee67bdc52b603035ef43d8c
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-08-07 11:15:19 +0000

    updated models.py

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
@@ -13,6 +13,6 @@ class Question(models.Model):
 
 class Anwser(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
