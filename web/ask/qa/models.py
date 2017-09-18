4c27de938107faf223d70dd8606433143e98a64d
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-09-18 17:49:05 +0300

    Update models.py

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -7,12 +7,27 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(
+        User, 
+        null=False, 
+        blank=False, 
+        on_delete=models.CASCADE
+    )
     likes = models.ManyToManyField(User, related_name='likes_set')
 
 
 class Anwser(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
-    question = models.ForeignKey(Question)
-    author = models.ForeignKey(User)
+    question = models.ForeignKey(
+        Question,
+        null=False, 
+        blank=False,
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User,
+        null=False, 
+        blank=False,
+        on_delete=models.CASCADE
+    )
