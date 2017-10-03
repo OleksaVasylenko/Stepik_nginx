5e81a28f5061408fbac08c4f12a900a8d663e56f
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-10-03 19:15:13 +0300

    Update models.py

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    
+    def popular(self):
+        return self.order_by('-rating')
+    
+    
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
