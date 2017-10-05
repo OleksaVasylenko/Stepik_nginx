709d5c814350ae133410e688c3c5e21114f0f45e
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-10-05 20:05:14 +0300

    Update utils.py

--- web/ask/qa/utils.py
+++ web/ask/qa/utils.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator, EmptyPage
 
 def paginate(request, query_set):
     try:
-	limit = int(request.GET.get('limit', 10)
-        page_num =int(request.GET.get('limit', 1)
+	limit = int(request.GET.get('limit', 10))
+        page_num =int(request.GET.get('limit', 1))
     except ValueError:
         limit = 10
         page_num = 1
