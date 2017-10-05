07db42169dd3d28ac42e686f7e0fb9b3d919ec39
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-10-05 10:51:12 +0000

    Add new_questions view

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from qa.models import Question
+
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def new_questions(request):
+    questions = Question.objects.new()
+    page_num = request.GET.get('page', 1)
+    limit = requset.GET.get('limit', 10)
+    paginator = Paginator(posts, limit)
+    page = paginator.page(page_num)
+    render(
+        request,
+        # template path,
+        {questions: page.object_list,
+         paginator: paginator,
+         page: page}
+    )
+    
